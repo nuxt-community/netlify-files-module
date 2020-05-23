7c47914bb95fcfd54f475400ba693f9e9aae3178
Andre Brdoch <andrebrdoch@yahoo.de>
2020-05-23 15:00:55 +0200

    test: updated basic test

--- test/basic.test.js
+++ test/basic.test.js
@@ -13,12 +13,14 @@ describe('basic', () => {
     await nuxt.close()
   })
 
-  test('it copies netlify files file', () => {
+  test('it copies netlify files', () => {
     const distBasePath = resolve(nuxt.options.rootDir, nuxt.options.generate.dir)
     const headersPath = join(distBasePath, '_headers')
     const redirectsPath = join(distBasePath, '_redirects')
+    const tomlPath = join(distBasePath, 'netlify.toml')
 
     expect(existsSync(headersPath)).toBe(true)
     expect(existsSync(redirectsPath)).toBe(true)
+    expect(existsSync(tomlPath)).toBe(true)
   })
 })
